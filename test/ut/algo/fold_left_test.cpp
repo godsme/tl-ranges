6b878424bed3fe123d08c1b8d3f8179aa471ceef
Darwin Yuan <darwin.yuan@hotmail.com>
2021-07-28 10:46:50 +0800

    added fold_left

--- test/ut/algo/fold_left_test.cpp
+++ test/ut/algo/fold_left_test.cpp
@@ -13,4 +13,32 @@ SCENARIO("fold_left") {
     }, tuple_c<int, long>);
 
     static_assert(result == size_c<sizeof(int) + sizeof(long)>);
+
+    constexpr auto result1 = fold_left(size_c<0>, [](auto&& acc, auto&& elem) {
+        return acc + elem.size();
+    })(tuple_c<int, long>);
+
+    static_assert(result1 == size_c<sizeof(int) + sizeof(long)>);
+}
+
+SCENARIO("fold_left_1") {
+    constexpr auto result = fold_left_1([](auto acc, auto elem) {
+        if constexpr(sizeof(typename decltype(acc)::type) > sizeof(typename decltype(elem)::type)) {
+            return acc;
+        } else {
+            return elem;
+        }
+    }, tuple_c<int, long, short, char>);
+
+    static_assert(result == type_c<long>);
+
+    constexpr auto result1 = fold_left_1([](auto acc, auto elem) {
+        if constexpr(sizeof(typename decltype(acc)::type) > sizeof(typename decltype(elem)::type)) {
+            return acc;
+        } else {
+            return elem;
+        }
+    })(tuple_c<int, long, short, char>);
+
+    static_assert(result1 == type_c<long>);
 }
