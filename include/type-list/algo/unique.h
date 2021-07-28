df1710f1a175d3facd2f4fa10e5ed0fd91372281
Darwin Yuan <darwin.yuan@hotmail.com>
2021-07-28 12:23:47 +0800

    refactored unique

--- include/type-list/algo/unique.h
+++ include/type-list/algo/unique.h
@@ -5,16 +5,22 @@
 #ifndef TYPE_LIST_2_CC85291452214D58ADB7E9D92CF99A08
 #define TYPE_LIST_2_CC85291452214D58ADB7E9D92CF99A08
 
-
 #include <type-list/algo/detail/apply_op.h>
-#include <type-list/algo/detail/unique_helper.h>
+#include <type-list/algo/fold_left.h>
+#include <type-list/algo/contains.h>
 #include <type-list/types/tuple_c.h>
 
 namespace holo::detail {
     struct unique {
         template <typename ... Xs>
         constexpr auto operator()(tuple_t<Xs...> xs) const -> auto {
-            return (unique_helper(tuple_c<>) << ... << tuple_elem<Xs>)();
+            return holo::fold_left(tuple_c<>, [](auto acc, auto elem) {
+                if constexpr(decltype(holo::contains(elem, acc))::value) {
+                    return acc;
+                } else {
+                    return tuple_cat(acc, elem);
+                }
+            }, xs);
         }
     };
 }
